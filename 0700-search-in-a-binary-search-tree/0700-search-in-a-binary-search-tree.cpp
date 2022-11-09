14c94ca237c2fc779a0892b7e98e07554c28f2d7
Diane Chaeyun Kim <104754233+golddohyun@users.noreply.github.com>
2022-11-09 22:37:25 +0900

    Update 0700-search-in-a-binary-search-tree.cpp

--- 0700-search-in-a-binary-search-tree/0700-search-in-a-binary-search-tree.cpp
+++ 0700-search-in-a-binary-search-tree/0700-search-in-a-binary-search-tree.cpp
@@ -9,14 +9,38 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+
+// Version 01. DFS - Recursion(BST)
 class Solution {
 public:
     TreeNode* searchBST(TreeNode* root, int val) {
         if (root == NULL) return NULL;
         if (val == root->val) return root;
-        if (val < root->val)
-            return searchBST(root->left, val);
-        else 
-            return searchBST(root->right, val);
+        if (val < root->val) return searchBST(root->left, val);
+        else return searchBST(root->right, val);
+    }
+};
+
+// Version 02. Iteration(BST)
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* searchBST(TreeNode* root, int val) {
+        if(root == NULL) return NULL;
+        while (root != NULL && root->val != val) {
+            if (val < root->val) root = root->left;
+            else root = root->right;
+        }
+        return root;
     }
 };
