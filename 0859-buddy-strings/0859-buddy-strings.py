bb54a4b39190cb411995732703e1ba7d3c39280e
Diane Chaeyun Kim <104754233+golddohyun@users.noreply.github.com>
2023-07-03 10:38:30 +0900

    Update 0859-buddy-strings.py

--- 0859-buddy-strings/0859-buddy-strings.py
+++ 0859-buddy-strings/0859-buddy-strings.py
@@ -5,3 +5,31 @@ class Solution(object):
             return len(set(A)) < len(A)
         pairs = [(a, b) for a, b in zip(A, B) if a != b]
         return len(pairs) == 2 and pairs[0] == pairs[1][::-1]
+
+
+""" 처음에 구구절절 짜서 한시간 날려먹은 코드
+테스트 케이스 24번째에서 막힘
+
+class Solution(object):
+    def buddyStrings(self, s, goal):
+        if len(s) != len(goal):
+            return False
+        if s == goal:
+            return len(set(s)) < len(s)
+        st = ['0' for _ in range(len(s))]
+        goal = list(goal)
+        for i in range(len(s)):
+            for j in range(i+1, len(goal)):
+                if s[i] == goal[i]:
+                    st[i] = s[i]
+                    continue
+                elif s[i] == goal[j] and s[j] == goal[i]:
+                    st[i] = s[j]
+                    st[j] = s[i]
+                    if st == goal: 
+                        return True
+                    else: 
+                        return False
+        
+        return False
+"""
