06b3b6f3bbb0422c797f782414e0f1197f59fa0e
Diane Chaeyun Kim <104754233+golddohyun@users.noreply.github.com>
2023-07-12 12:28:03 +0900

    Update 0802-find-eventual-safe-states.py

--- 0802-find-eventual-safe-states/0802-find-eventual-safe-states.py
+++ 0802-find-eventual-safe-states/0802-find-eventual-safe-states.py
@@ -25,3 +25,27 @@ class Solution:
                 safe_nodes.append(node)
 
         return safe_nodes
+
+
+## Time limit exceeded version
+# class Solution:
+#     def dfs(self, graph, root) :
+#         visited = []
+#         stack = [(root, [])]
+#         while stack:
+#             node, path = stack.pop()
+#             if node in path:
+#                 return False
+#             if node not in visited:
+#                 visited.append(node)
+#             # print('modified visited queue', visited)
+#                 stack.extend((child, path + [node]) for child in graph[node])
+#             # print('modified stack', stack)
+#         return True
+
+
+#     def eventualSafeNodes(self, graph: List[List[int]]) -> List[int]:
+#         safenode = []
+#         for i in range(len(graph)) :
+#             if self.dfs(graph,i) : safenode.append(i)
+#         return safenode
