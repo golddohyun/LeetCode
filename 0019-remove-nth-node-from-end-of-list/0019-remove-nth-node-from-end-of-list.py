d8cd06d1cd4b23bf8527066bdf8e88f979a3566b
Diane Chaeyun Kim <104754233+golddohyun@users.noreply.github.com>
2023-08-06 20:10:26 +0900

    Time: 57 ms (10.34%), Space: 16.6 MB (38.78%) - LeetHub

--- 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.py
+++ 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.py
@@ -1,21 +1,24 @@
-# Definition for singly-linked list.
-# class ListNode:
-#     def __init__(self, val=0, next=None):
-#         self.val = val
-#         self.next = next
 class Solution:
-    def removeNthFromEnd(self, head: ListNode, n: int) -> ListNode:
-        ptr_tmp, length = head, 0
+    def removeNthFromEnd(self, head: Optional[ListNode], n: int) -> Optional[ListNode]:
+        reversed_head = self.reverse_ll(head)
+        prev = None
+        curr = reversed_head
 
-        ## Go to the end of the linked list
-        while ptr_tmp :
-            ptr_tmp = ptr_tmp.next
-            length += 1
-        ## Edge case 
-        if length == n : return head.next
-        ## Second pointer
-        ptr = head
-        for i in range(length - n -1) :
-            ptr = ptr.next
-        ptr.next = ptr.next.next
-        return head
+        for _ in range(n - 1):
+            prev = curr
+            curr = curr.next
+        if prev != None:
+            prev.next = curr.next
+        else:
+            reversed_head = curr.next
+        return self.reverse_ll(reversed_head)
+
+    def reverse_ll(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        curr = copy.deepcopy(head)
+        prev = None
+        while curr:
+            tmp = curr.next
+            curr.next = prev
+            prev = curr
+            curr = tmp
+        return prev
