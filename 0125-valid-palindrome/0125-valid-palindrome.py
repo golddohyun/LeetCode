b432332949ff6ecdc62156e2196b06c675a4d35d
Diane Chaeyun Kim <104754233+golddohyun@users.noreply.github.com>
2023-11-29 09:36:46 +0900

    Time: 774 ms (5.01%), Space: 14.8 MB (52.60%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.py
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -1,14 +1,11 @@
+## valid palindrome 1
 class Solution(object):
     def isPalindrome(self, s):
-        """
-        :type s: str
-        :rtype: bool
-        """        
         s = s.lower()
         for i in s:
             if False == i.isalnum():
                 s = s.replace(i,"")
-
+				# 다른 방법 : s = ''.join(i for i in s if i.isalnum()).lower()
         def palindorme(s):
             idx = len(s)
             if(idx%2): # odd number
