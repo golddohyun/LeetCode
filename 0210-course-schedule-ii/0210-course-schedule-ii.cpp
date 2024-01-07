a24492c109f4a1cf31ce6021da199b2dbc51a676
Diane Chaeyun Kim <104754233+golddohyun@users.noreply.github.com>
2024-01-07 16:07:40 +0900

    Time: 21 ms (39.57%), Space: 13.7 MB (85.50%) - LeetHub

---
+++ 0210-course-schedule-ii/0210-course-schedule-ii.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) {
+        vector<vector<int>> adj(numCourses);
+        vector<int> deg(numCourses, 0);
+        vector<int> result;
+        // get adjacent list and indegree list
+        for(auto& i : prerequisites){
+            adj[i[0]].push_back(i[1]);
+            deg[i[1]]++;
+        }
+        queue<int> Q;
+        for (int i =0; i < numCourses; i++){
+            if(deg[i] == 0) Q.push(i);
+        }
+        
+        for(int i = 0; i < numCourses; i++){
+
+            //cycle exists
+            if (Q.empty()){
+                return {};
+            }
+            int x = Q.front();
+            Q.pop();
+            result.push_back(x);
+            for (int i=0; i < adj[x].size(); i++){
+                int y = adj[x][i];
+                if (--deg[y] == 0) Q.push(y);
+            }
+        }
+        reverse(result.begin(), result.end());
+        return result;
+    }
+};
