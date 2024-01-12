19e7e7f2da3976720244cd78854f9cac2ed7e616
Diane Chaeyun Kim <104754233+golddohyun@users.noreply.github.com>
2024-01-12 23:06:23 +0900

    Time: 25 ms (21.31%), Space: 13.4 MB (99.49%) - LeetHub

--- 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.py
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.py
@@ -4,5 +4,18 @@ class Solution(object):
         :type s: str
         :rtype: str
         """
-        return ' '.join(s.strip().split()[::-1])
+        # return ' '.join(s.strip().split()[::-1])
+
+        ans = []
+        tmp = ""
+        for let in s:
+            if let != " ":
+                tmp += let
+            elif tmp:
+                ans.append(tmp)
+                tmp = ""
+        # Add the last word if it exists
+        if tmp:
+            ans.append(tmp)
+        return ' '.join(ans[::-1])
         
