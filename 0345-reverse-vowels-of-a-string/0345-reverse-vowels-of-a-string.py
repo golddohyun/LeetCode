af6e7147a9910c911ca61f336e7e915145de3c1b
Diane Chaeyun Kim <104754233+golddohyun@users.noreply.github.com>
2024-01-12 22:49:37 +0900

    Time: 29 ms (90.87%), Space: 15.4 MB (75.75%) - LeetHub

--- 0345-reverse-vowels-of-a-string/0345-reverse-vowels-of-a-string.py
+++ 0345-reverse-vowels-of-a-string/0345-reverse-vowels-of-a-string.py
@@ -4,11 +4,23 @@ class Solution(object):
         :type s: str
         :rtype: str
         """
+        ## runtime 36ms (faster than 78%), Too much memory usage
+        # idxlst = [i for i in range(len(s)) if s[i] in "aeiouAEIOU"]
+        # rev = [i for i in s if i in "aeiouAEIOU"][::-1]
+        # anslst = list(s)
+        # for i in range(len(idxlst)) :
+            # anslst[idxlst[i]] = rev[i]
+        # return ''.join(anslst)
 
-        idxlst = [i for i in range(len(s)) if s[i] in "aeiouAEIOU"]
-        rev = [i for i in s if i in "aeiouAEIOU"][::-1]
-        anslst = list(s)
-        for i in range(len(idxlst)) :
-            anslst[idxlst[i]] = rev[i]
-        return ''.join(anslst)
-        
+        left, right, slist = 0, len(s)-1, list(s)
+        vowels=set('AEIOUaeiou')
+        while left < right :
+            if left < right and s[left] not in vowels :
+                left +=1
+            elif left < right and s[right] not in vowels :
+                right -=1
+            else :
+                slist[left], slist[right] = slist[right], slist[left]
+                left+=1
+                right -=1
+        return ''.join(slist)
