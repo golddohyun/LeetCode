e38bfc9a209cdd15316e0f4e30deb2dabc15598d
Diane Chaeyun Kim <104754233+golddohyun@users.noreply.github.com>
2024-01-12 22:02:09 +0900

    Time: 10 ms (90.67%), Space: 13.3 MB (88.27%) - LeetHub

--- 1768-merge-strings-alternately/1768-merge-strings-alternately.py
+++ 1768-merge-strings-alternately/1768-merge-strings-alternately.py
@@ -5,15 +5,18 @@ class Solution(object):
         :type word2: str
         :rtype: str
         """
-        s = min(len(word1), len(word2))
-        newstr=''
+#         s = min(len(word1), len(word2))
+#         newstr=''
 
-        for i in range(s) :
-            newstr+= word1[i]
-            newstr+= word2[i]
-        if len(word1) < len(word2) :
-            newstr +=word2[s:]
-        elif len(word2) < len(word1) :
-            newstr += word1[s:]
+#         for i in range(s) :
+#             newstr+= word1[i]
+#             newstr+= word2[i]
+#         if len(word1) < len(word2) :
+#             newstr +=word2[s:]
+#         elif len(word2) < len(word1) :
+#             newstr += word1[s:]
+        s, newstr = min(len(word1), len(word2)), ''
+        newstr = ''.join(word1[i] + word2[i] for i in range(s))
+        newstr += word1[s:] + word2[s:]
 
         return newstr
