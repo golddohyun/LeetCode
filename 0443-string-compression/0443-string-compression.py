2401b46d6a8c4c076feaa63019acaafd48068a72
Diane Chaeyun Kim <104754233+golddohyun@users.noreply.github.com>
2024-01-15 10:40:32 +0900

    Time: 33 ms (76.97%), Space: 13.7 MB (14.63%) - LeetHub

--- 0443-string-compression/0443-string-compression.py
+++ 0443-string-compression/0443-string-compression.py
@@ -1,20 +1,51 @@
-class Solution:
-    def compress(self, chars: List[str]) -> int:
-        stack=[]
-        s=""
-        for i in range(len(chars)):
-            if len(stack)==0 or stack[-1]==chars[i]:
-                stack.append(chars[i])
+class Solution(object):
+    def compress(self, chars):
+        """
+        :type chars: List[str]
+        :rtype: int
+        """
+        if len(chars) == 1:
+            return len(chars)
+
+        left, right = 0, 0
+        num = 0
+        while right < len(chars):
+            if (chars[left] != chars[right]):
+                ## insert the num right next to the alphabet
+                if num == 1:
+                    left = right
+                else:
+                    num_str = str(num)
+                    chars[left + 1] = num_str[0]
+                    if len(num_str) > 1:
+                        for i in range(1, len(num_str)):
+                            chars.insert(left + 1 + i, num_str[i])
+                        ## erase other alphabet in between (left+ lenstr +1 < idx < right)
+                        del chars[left + len(num_str) + 1:right+len(num_str)-1]
+                        right = left = left + len(num_str) + 1
+                    else:
+                        if num > 2:
+                            del chars[left + 2:right]
+                            right = left = left + 2
+                        else:
+                            left = right
+                num = 0
             else:
-                s+=stack[-1]
-                if len(stack)!=1:
-                    s+=str(len(stack))
-                stack.clear()
-                stack.append(chars[i])
-        s+=stack[-1]
-        if len(stack)!=1:
-            s+=str(len(stack))
-        chars.clear()
-        for i in s:
-            chars.append((i))
-        return len(s)
+                num += 1
+                right += 1
+
+        # 마지막 후처리
+        if right == len(chars):
+            num_str = str(num)
+            if num > 1:
+                chars[left + 1] = num_str[0]
+                if len(num_str) > 1:
+                    for i in range(1, len(num_str)):
+                        chars.insert(left + 1 + i, num_str[i])
+                    del chars[left + len(num_str) + 1:right+len(num_str)-1]
+                else:
+                    if num > 2:
+                        del chars[left + 2:right]
+        
+
+        return len(chars)
