d0fce6dd8770fedb3d453d79a6ece4c92fc92333
Diane Chaeyun Kim <104754233+golddohyun@users.noreply.github.com>
2024-01-22 15:57:15 +0900

    Time: 782 ms (5.22%), Space: 12.3 MB (100.00%) - LeetHub

--- 0784-letter-case-permutation/0784-letter-case-permutation.py
+++ 0784-letter-case-permutation/0784-letter-case-permutation.py
@@ -1,20 +1,29 @@
 class Solution(object):
+    def swapcase(self, data, idx) :
+        if data[idx].islower() :
+            data[idx] = data[idx].upper()
+        else :
+            data[idx] = data[idx].lower()
     def permute(self, data, i, length, result):
-        if i == length:
+        if i == length :
             result.append(''.join(data))
-        else:
-            # Proceed without changing the current character
+        else :
+            # Include the current character as is
             self.permute(data, i + 1, length, result)
-            if data[i].isalpha():
-                data[i] = data[i].swapcase() 
-                self.permute(data, i + 1, length, result)
-                data[i] = data[i].swapcase()  # Revert the case change
 
+            if data[i].isalpha() :
+                self.swapcase(data, i)
+                self.permute(data, i+1, length, result)
+                self.swapcase(data, i)
+                
+        return list(set(result))          
     def letterCasePermutation(self, s):
         """
         :type s: str
         :rtype: List[str]
         """
-        ans = []
-        self.permute(list(s), 0, len(s), ans)
+        if len(s) == 1 :
+            if s[0].isalpha() :
+                return [s.upper(), s.lower()]
+        ans = self.permute(list(s), 0, len(s), [])
         return ans
