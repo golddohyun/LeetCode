30c40cb50de14655a83cc84f82fcfb1d9ceb430b
Diane Chaeyun Kim <104754233+golddohyun@users.noreply.github.com>
2024-01-22 17:24:31 +0900

    Time: 47 ms (31.98%), Space: 16.7 MB (57.68%) - LeetHub

--- 0090-subsets-ii/0090-subsets-ii.py
+++ 0090-subsets-ii/0090-subsets-ii.py
@@ -1,11 +1,23 @@
 class Solution:
+    # def backtrack_comb(self, nums, idx, k, used, arr, result):
+    #     if len(arr)==k and arr not in result:
+    #         result.append(arr)
+    #     for i in range(idx+1, len(nums)):
+    #         if used[i]==False:
+    #             used[i] = True
+    #             self.backtrack_comb(nums, i, k, used, arr+[nums[i]], result)
+    #             used[i] = False
+    #     return result
+
     def backtrack_comb(self, nums, idx, k, used, arr, result):
-        if len(arr)==k and arr not in result:
-            result.append(arr)
-        for i in range(idx+1, len(nums)):
-            if used[i]==False:
+        if len(arr) == k and arr not in result:
+            result.append(arr.copy()) 
+        for i in range(idx, len(nums)):
+            if not used[i]:
                 used[i] = True
-                self.backtrack_comb(nums, i, k, used, arr+[nums[i]], result)
+                arr.append(nums[i])
+                self.backtrack_comb(nums, i+1, k, used, arr, result)
+                arr.pop() 
                 used[i] = False
         return result
 
@@ -13,9 +25,11 @@ class Solution:
         leng = []
         used = [False for i in range(len(nums))]
         for k in range(len(nums)+1) :
-            for elem in self.backtrack_comb(nums, -1, k, used, [], []) :
+            for elem in self.backtrack_comb(nums, 0, k, used, [], []) :
                 elem.sort()
                 if elem not in leng :
                     leng.append(elem)
+
+        return leng
 
         return leng
