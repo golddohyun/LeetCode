589baeba5721ecbf796616164c53eb030d0eaca9
Diane Chaeyun Kim <104754233+golddohyun@users.noreply.github.com>
2024-01-30 08:57:51 +0900

    Time: 104 ms (40.50%), Space: 12 MB (97.52%) - LeetHub

--- 1254-number-of-closed-islands/1254-number-of-closed-islands.py
+++ 1254-number-of-closed-islands/1254-number-of-closed-islands.py
@@ -1,5 +1,3 @@
-## Memory Usage: 13.4 MB, less than 99.00%
-## Runtime: 106 ms, faster than 39.00%
 class Solution(object):
     def closedIsland(self, grid):
         """
@@ -7,30 +5,36 @@ class Solution(object):
         :rtype: int
         """
         from collections import deque
-        def bfs(grid, queue):
+        def bfs(queue) :
             directions = [(0,1),(0,-1),(1,0),(-1,0)]
-            while queue : # while queue is not empty
+            while queue :
                 curx, cury = queue.popleft()
                 for dr, dc in directions :
-                    nx, ny = curx +dr, cury+dc
-                    if (nx < 0 or ny < 0 or nx >= len(grid) or ny >= len(grid[0])): continue
-                    if (grid[nx][ny] != 0) : continue
-                    grid[nx][ny] = -2
+                    nx, ny = curx+dr, cury+dc
+                    if (nx < 0 or ny < 0  or nx >= len(grid) or ny >= len(grid[0])) : continue
+                    if (grid[nx][ny] != 0 ) : continue
                     queue.append([nx, ny])
-
-        q, cnt = deque(), 0
+                    grid[nx][ny] = -2
+        
+        # initialize 
         rows, cols = len(grid), len(grid[0])
-        for r in range(rows):
-            for c in range(cols) :
-                if (r in [0, rows-1] and grid[r][c] ==0) or (c in [0, cols-1] and grid[r][c] ==0) :
-                    q.append([r, c])
-                    bfs(grid, q)
+        queue, cnt = deque(), 0
 
-        for r in range(1, rows-1) :
-            for c in range(1, cols-1) :
-                if grid[r][c] == 0 : # if not visited and # of grid is 0
-                    q.append([r, c])
-                    bfs(grid, q)
-                    # print(r, c)
-                    cnt+=1
+        ## first visit the grid at the first and the last axis & value of grid == 0 
+        for r in range(rows) :
+            for c in range(cols) :
+                if (r in [0, rows-1] or c in [0, cols-1]) and grid[r][c] == 0 :
+                    queue.append([r, c])
+                    grid[r][c] = -2 # marked as visited
+                    bfs(queue)
+
+        ## run bfs within valid choices
+        for r in range(rows) :
+            for c in range(cols) :
+                if grid[r][c] == 0 :
+                    queue.append([r, c])
+                    grid[r][c] = -2
+                    bfs(queue)
+                    cnt +=1
         return cnt
+        
