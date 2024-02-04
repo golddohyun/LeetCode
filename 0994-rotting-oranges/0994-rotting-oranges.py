f1ce2c3ab1c939aec17a0c53ec22ada295dcccc9
Diane Chaeyun Kim <104754233+golddohyun@users.noreply.github.com>
2024-02-04 12:35:39 +0900

    Time: 31 ms (63.88%), Space: 11.8 MB (78.87%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.py
+++ 0994-rotting-oranges/0994-rotting-oranges.py
@@ -4,41 +4,29 @@ class Solution(object):
         :type grid: List[List[int]]
         :rtype: int
         """
-        
-        if len(grid) == 0 : return -1
-        
-        queue, maxval = deque(), 0
-        rows, cols = len(grid), len(grid[0])
+        row, col = len(grid), len(grid[0])
         directions = [(0,1),(0,-1),(1,0),(-1,0)]
+        visited = [[0]*col for _ in range(row)]
+        maxdst, queue = -1, deque()
 
-
-        ## initialize : put grids that's rotten
-        zero_flag = False
-        for r in range(rows) :
-            for c in range(cols) :
+        for r in range(row) :
+            for c in range(col) :
                 if grid[r][c] == 2 :
-                    queue.append([r, c])
-                    zero_flag = True
-                elif grid[r][c] == 1 :
-                    zero_flag = True
-        
+                    queue.append([r, c, 0]) # x, y, distance
+                    visited[r][c] = -1
+
         while queue :
-            curx, cury = queue.popleft()
+            curx, cury, dist = queue.popleft()
+            maxdst = max(maxdst, dist)
             for dr, dc in directions :
-                nx, ny = curx + dr, cury + dc
-                if (nx < 0 or ny < 0 or nx >= rows or ny >= cols) : continue
-                if grid[nx][ny] != 1 : continue # already rotten or is empty
-                queue.append([nx, ny])
-                grid[nx][ny] += grid[curx][cury]
-            if maxval < grid[curx][cury] :
-                maxval = grid[curx][cury]
+                nx, ny = curx+dr, cury+dc
+                if (0 <= nx < row and 0 <= ny < col and visited[nx][ny] == 0 and grid[nx][ny] == 1) :
+                    queue.append([nx, ny, dist+1])
+                    visited[nx][ny] = 1
 
-        # filtering
-        if not zero_flag : return 0
-        if maxval == 0 : return -1
-        for items in grid :
-            if 1 in items :
-                return -1
-        return maxval-2
+        visit_1, grid_1 = sum(row.count(1) for row in visited),  sum(row.count(1) for row in grid)
 
+        if visit_1 != grid_1 : return -1
+        if visit_1 == 0 : return 0
 
+        return maxdst
