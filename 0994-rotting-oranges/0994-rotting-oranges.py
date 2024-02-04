2f72e7b2ff1abcdab1ed9ddf6d71caf8c891fc69
Diane Chaeyun Kim <104754233+golddohyun@users.noreply.github.com>
2024-02-04 12:39:52 +0900

    Time: 35 ms (39.85%), Space: 11.6 MB (95.61%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.py
+++ 0994-rotting-oranges/0994-rotting-oranges.py
@@ -1,32 +1,28 @@
 class Solution(object):
     def orangesRotting(self, grid):
-        """
-        :type grid: List[List[int]]
-        :rtype: int
-        """
         row, col = len(grid), len(grid[0])
         directions = [(0,1),(0,-1),(1,0),(-1,0)]
-        visited = [[0]*col for _ in range(row)]
-        maxdst, queue = -1, deque()
+        queue = deque()
+        fresh_oranges = 0
 
-        for r in range(row) :
-            for c in range(col) :
-                if grid[r][c] == 2 :
-                    queue.append([r, c, 0]) # x, y, distance
-                    visited[r][c] = -1
+        for r in range(row):
+            for c in range(col):
+                if grid[r][c] == 2:
+                    queue.append((r, c, 0)) # x, y, distance
+                elif grid[r][c] == 1:
+                    fresh_oranges += 1
 
-        while queue :
+        max_dist = 0
+        while queue:
             curx, cury, dist = queue.popleft()
-            maxdst = max(maxdst, dist)
-            for dr, dc in directions :
-                nx, ny = curx+dr, cury+dc
-                if (0 <= nx < row and 0 <= ny < col and visited[nx][ny] == 0 and grid[nx][ny] == 1) :
-                    queue.append([nx, ny, dist+1])
-                    visited[nx][ny] = 1
+            max_dist = max(max_dist, dist)
+            for dr, dc in directions:
+                nx, ny = curx + dr, cury + dc
+                if 0 <= nx < row and 0 <= ny < col and grid[nx][ny] == 1:
+                    grid[nx][ny] = 2 # Mark as rotten
+                    fresh_oranges -= 1
+                    queue.append((nx, ny, dist + 1))
 
-        visit_1, grid_1 = sum(row.count(1) for row in visited),  sum(row.count(1) for row in grid)
-
-        if visit_1 != grid_1 : return -1
-        if visit_1 == 0 : return 0
-
-        return maxdst
+        if fresh_oranges > 0:
+            return -1
+        return max_dist
