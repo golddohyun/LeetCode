644a24989bc73d89955a87469e6fad14e6f3171e
Diane Chaeyun Kim <104754233+golddohyun@users.noreply.github.com>
2024-02-04 12:03:14 +0900

    Time: 684 ms (23.94%), Space: 13.8 MB (84.51%) - LeetHub

--- 1926-nearest-exit-from-entrance-in-maze/1926-nearest-exit-from-entrance-in-maze.py
+++ 1926-nearest-exit-from-entrance-in-maze/1926-nearest-exit-from-entrance-in-maze.py
@@ -1,29 +1,20 @@
 class Solution(object):
     def nearestExit(self, maze, entrance):
-        """
-        :type maze: List[List[str]]
-        :type entrance: List[int]
-        :rtype: int
-        """
         from collections import deque
         row, col = len(maze), len(maze[0])
         directions = [(0,1),(0,-1),(1,0),(-1,0)]
-        queue = deque([entrance])
-        maze[entrance[0]][entrance[1]] = 0
-        min_cnt= float('inf')
-        while queue :
-            curx, cury = queue.popleft()
-            if (curx in [0, row-1] or cury in [0, col-1]) and [curx, cury] != entrance :
-                min_cnt = min(min_cnt, maze[curx][cury])
-            for dr, dc in directions :
-                nx, ny = curx+dr, cury+dc
-                if (nx < 0 or ny < 0 or nx >=row or ny >=col) : continue
-                if maze[nx][ny] != "." : continue
-                queue.append([nx,ny])
-                maze[nx][ny] = maze[curx][cury]+1
-
-
-        if min_cnt == float('inf') :
-            return -1
-
-        return min_cnt
+        queue = deque([(entrance[0], entrance[1], 0)]) # x, y, distance
+        maze[entrance[0]][entrance[1]] = "F" # Marked as visited
+        min_dist = float('inf')
+        
+        while queue:
+            curx, cury, dist = queue.popleft()
+            if (curx in [0, row-1] or cury in [0, col-1]) and [curx, cury] != entrance:
+                min_dist = min(min_dist, dist)
+            for dr, dc in directions:
+                nx, ny = curx + dr, cury + dc
+                if 0 <= nx < row and 0 <= ny < col and maze[nx][ny] == ".":
+                    queue.append((nx, ny, dist + 1))
+                    maze[nx][ny] = "F"
+        
+        return min_dist if min_dist != float('inf') else -1
