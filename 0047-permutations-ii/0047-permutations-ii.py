536465c91ec850e6d59b511e920546340a5b4e83
Diane Chaeyun Kim <104754233+golddohyun@users.noreply.github.com>
2024-02-07 16:05:35 +0900

    Time: 35 ms (75.51%), Space: 12.2 MB (75.39%) - LeetHub

--- 0047-permutations-ii/0047-permutations-ii.py
+++ 0047-permutations-ii/0047-permutations-ii.py
@@ -1,26 +1,19 @@
-class Solution(object):
+
+class Solution:
     def permuteUnique(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: List[List[int]]
-        """
-        N = len(nums)
-        used = [0]*N
-
-        def backtrack_permute(arr, result):
-            if len(arr) == N and arr not in result :
-                result.append(arr[:])
-                return 
-
-            for i in range(N) :
-                if not used[i] :
-                    arr.append(nums[i])
-                    used[i] = 1
-                    backtrack_permute(arr, result)
-                    used[i] = 0
-                    arr.pop()
         
-        nums.sort()
-        result = []
-        backtrack_permute([],result)
-        return result
+        permutations = []
+        counter = Counter(nums)
+        def findAllPermutations(res):
+            if len(res) == len(nums):
+                permutations.append(res)
+                return 
+            
+            for key in counter:
+                if counter[key]:
+                    counter[key]-=1 # decrement visited key
+                    findAllPermutations(res + [key])    
+                    counter[key]+=1 # restore the state of visited key to find the next path
+                
+        findAllPermutations([])
+        return permutations
