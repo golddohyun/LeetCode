02ba46f3c01482b2cf8a0b15d7f977c378acd571
Diane Chaeyun Kim <104754233+golddohyun@users.noreply.github.com>
2024-02-09 08:45:06 +0900

    Time: 35 ms (74.84%), Space: 11.6 MB (90.41%) - LeetHub

--- 0040-combination-sum-ii/0040-combination-sum-ii.py
+++ 0040-combination-sum-ii/0040-combination-sum-ii.py
@@ -11,11 +11,11 @@ class Solution(object):
                 return
             prev = -1
             for i in range(start, len(candidates)) :
-                if i > start and candidates[i] == prev :
+                if candidates[i] == prev :
                     continue
-                if candidates[i] > target: break
-                prev = candidates[i]
-                backtrack_comb(i+1, arr+[candidates[i]], target-candidates[i])
+                if candidates[i] <= target:
+                    prev = candidates[i]
+                    backtrack_comb(i+1, arr+[candidates[i]], target-candidates[i])
 
         result = []
         candidates.sort()
