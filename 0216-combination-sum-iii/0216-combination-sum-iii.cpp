3a3a54605736294cdc845e6174aef67017c34bcc
Diane Chaeyun Kim <104754233+golddohyun@users.noreply.github.com>
2024-02-09 15:27:35 +0900

    Time: 3 ms (47.55%), Space: 8.2 MB (22.26%) - LeetHub

---
+++ 0216-combination-sum-iii/0216-combination-sum-iii.cpp
@@ -0,0 +1,22 @@
+
+class Solution {
+public:
+    void backtrack_comb(int start, const int& k, int target, vector<int>& arr, vector<vector<int>>& res){
+        if (arr.size() == k && target == 0) {
+            res.push_back(arr);
+            return;
+        }
+        for (int num=start; num < 10; num++){
+            arr.push_back(num);
+            backtrack_comb(num+1, k, target-num, arr, res);
+            arr.pop_back();
+        }
+
+    }
+    vector<vector<int>> combinationSum3(int k, int n) {
+        vector<vector<int>> res;
+        vector<int> arr;
+        backtrack_comb(1, k, n, arr, res);
+        return res;
+    }
+};
