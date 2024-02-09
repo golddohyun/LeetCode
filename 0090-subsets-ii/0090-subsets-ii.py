f2c6674b887b2b48f1167fd9f7f8f8a145acd617
Diane Chaeyun Kim <104754233+golddohyun@users.noreply.github.com>
2024-02-09 16:17:51 +0900

    Time: 169 ms (5.11%), Space: 17.5 MB (41.35%) - LeetHub

--- 0090-subsets-ii/0090-subsets-ii.py
+++ 0090-subsets-ii/0090-subsets-ii.py
@@ -1,35 +1,18 @@
 class Solution:
-    # def backtrack_comb(self, nums, idx, k, used, arr, result):
-    #     if len(arr)==k and arr not in result:
-    #         result.append(arr)
-    #     for i in range(idx+1, len(nums)):
-    #         if used[i]==False:
-    #             used[i] = True
-    #             self.backtrack_comb(nums, i, k, used, arr+[nums[i]], result)
-    #             used[i] = False
-    #     return result
-
-    def backtrack_comb(self, nums, idx, k, used, arr, result):
-        if len(arr) == k and arr not in result:
-            result.append(arr.copy()) 
-        for i in range(idx, len(nums)):
-            if not used[i]:
-                used[i] = True
-                arr.append(nums[i])
-                self.backtrack_comb(nums, i+1, k, used, arr, result)
-                arr.pop() 
-                used[i] = False
-        return result
-
     def subsetsWithDup(self, nums) :
-        leng = []
-        used = [False for i in range(len(nums))]
+        def backtrack_comb(start, arr, mx_len) :
+            if len(arr) == mx_len :
+                result.append(arr)
+                print(result)
+                return
+            prev = -1
+            for idx in range(start, len(nums)) :
+                if (idx > start and prev == nums[idx]) : continue
+                prev = nums[idx]
+                backtrack_comb(idx+1, arr+[nums[idx]], mx_len)
+        
+        result = []
+        nums.sort()
         for k in range(len(nums)+1) :
-            for elem in self.backtrack_comb(nums, 0, k, used, [], []) :
-                elem.sort()
-                if elem not in leng :
-                    leng.append(elem)
-
-        return leng
-
-        return leng
+            backtrack_comb(0, [], k)
+        return sorted(result)
