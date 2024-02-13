37f6fde33900984f9cf0d5a2e0846e9a937d7128
Diane Chaeyun Kim <104754233+golddohyun@users.noreply.github.com>
2024-02-14 08:45:10 +0900

    Time: 752 ms (5.03%), Space: 13.3 MB (78.72%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.py
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -1,19 +1,30 @@
 ## valid palindrome 1
 class Solution(object):
     def isPalindrome(self, s):
+        def palindrome(st) :
+            start, end= 0, len(st)-1
+            while start < end :
+                if st[start] != st[end] : return False
+                start+=1
+                end-=1
+            return True
+        
         s = s.lower()
+
         for i in s:
             if False == i.isalnum():
                 s = s.replace(i,"")
 				# 다른 방법 : s = ''.join(i for i in s if i.isalnum()).lower()
-        def palindorme(s):
-            idx = len(s)
-            if(idx%2): # odd number
-                if s[0:idx//2] == s[idx:idx//2:-1]:
-                    return True
-            else:
-                if s[0:idx//2] == s[idx:idx//2-1:-1]:
-                    return True
-            return False
 
-        return palindorme(s)
+        # def palindrome(s):
+        #     idx = len(s)
+        #     if(idx%2): # odd number
+        #         if s[0:idx//2] == s[idx:idx//2:-1]:
+        #             return True
+        #     else:
+        #         if s[0:idx//2] == s[idx:idx//2-1:-1]:
+        #             return True
+        #     return False
+
+
+        return palindrome(s)
