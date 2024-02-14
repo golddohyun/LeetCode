b77d8626752b6b9696a34f5a7e5eec1dec566462
Diane Chaeyun Kim <104754233+golddohyun@users.noreply.github.com>
2024-02-14 14:14:08 +0900

    Time: 12 ms (35.88%), Space: 5.9 MB (78.22%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.c
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.c
@@ -13,23 +13,21 @@ bool isSubstring(char ch, char* str) {
 int lengthOfLongestSubstring(char* s) {
     int len_s = strlen(s);
     int curmax = 0;
-    if (len_s < 2) {return len_s;}
+    if (len_s < 2) return len_s;
 
-    for (int i=0; i < len_s; i++) {
-        char* tmp = (char*)malloc(len_s + 1); 
-        memset(tmp, 0, len_s + 1); // initialize to zero
+    for (int i = 0; i < len_s; i++) {
+        bool exist[256] = {false}; // ASCII 문자의 존재 여부를 추적하는 배열
         int ptr = 0;
-        for (int cur=i; cur < len_s; cur++) {
-            if (isSubstring(s[cur], tmp)) {
+        for (int cur = i; cur < len_s; cur++) {
+            if (exist[(unsigned char)s[cur]]) {
                 break;
             }
-            tmp[ptr++] = s[cur]; 
+            exist[(unsigned char)s[cur]] = true;
+            ptr++;
         }
-        tmp[ptr] = '\0'; 
-        if (ptr > curmax) { 
-            curmax = ptr; 
+        if (ptr > curmax) {
+            curmax = ptr;
         }
-        free(tmp); 
     }
     return curmax;
 }
