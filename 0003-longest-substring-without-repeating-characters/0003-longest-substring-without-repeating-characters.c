02b89338df227e75d2dbea13c83cb723c1308880
Diane Chaeyun Kim <104754233+golddohyun@users.noreply.github.com>
2024-02-14 14:30:25 +0900

    Time: 12 ms (35.88%), Space: 5.7 MB (89.84%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.c
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.c
@@ -9,7 +9,7 @@ int lengthOfLongestSubstring(char* s) {
     if (len_s < 2) return len_s;
 
     for (int i = 0; i < len_s; i++) {
-        bool exist[256] = {false}; // ASCII 문자의 존재 여부를 추적하는 배열
+        bool exist[128] = {false}; // ASCII 문자의 존재 여부를 추적하는 배열
         int ptr = 0;
         for (int cur = i; cur < len_s; cur++) {
             if (exist[(unsigned char)s[cur]]) {
