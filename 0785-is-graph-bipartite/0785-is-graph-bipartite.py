41e6b37d1b7667f5093aaada4393ff83be4e5497
Diane Chaeyun Kim <104754233+golddohyun@users.noreply.github.com>
2024-02-23 17:54:36 +0900

    Time: 312 ms (5.20%), Space: 12 MB (86.00%) - LeetHub

--- 0785-is-graph-bipartite/0785-is-graph-bipartite.py
+++ 0785-is-graph-bipartite/0785-is-graph-bipartite.py
@@ -6,27 +6,22 @@ class Solution(object):
         """
         from collections import deque
         for V in range(len(graph)) :
-            red = [0]*len(graph)
-            blue = [0]*len(graph)
+            color = [-1]*len(graph)
             visited = [0]*len(graph)
-
-            # starting point = V
             Q = deque([V])
-            red[V] = 1
-
+            color[V] = 1
             while Q :
                 cur = Q.popleft()
-                visited[cur] =1
+                visited[cur] = 1
                 for nei in graph[cur] : 
-                    if (not visited[nei] and red[cur] ==1 and blue[nei] == 0) : 
+                    if (not visited[nei] and color[cur] == 1 and color[nei] ==-1) : 
                         Q.append(nei)
-                        blue[nei] = 1
+                        color[nei] = 0
                         visited[nei] =1
-                    elif (not visited[nei] and  blue[cur]==1 and red[nei]== 0) :
+                    elif (not visited[nei] and  color[cur]==0  and color[nei]== -1) :
                         Q.append(nei)
-                        red[nei] =1
+                        color[nei] =1
                         visited[nei] =1
-                    elif (red[nei] == red[cur] or blue[nei] == blue[cur]) :
+                    elif (color[cur]!= -1 and color[nei] == color[cur]) :
                         return False
-
         return True
