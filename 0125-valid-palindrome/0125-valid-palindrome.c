639960b827f06133f150947ffe9ba6358dadc63b
Diane Chaeyun Kim <104754233+golddohyun@users.noreply.github.com>
2024-02-25 15:03:30 +0900

    Time: 6 ms (49.39%), Space: 9.2 MB (13.18%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.c
+++ 0125-valid-palindrome/0125-valid-palindrome.c
@@ -1,40 +1,33 @@
-#include <stdio.h>
-#include <string.h>
-#include <stdbool.h>
-#include <stdlib.h>
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        string newstr = "";
+        for (auto let : s){
+            if ((let >='a' && let <= 'z') || (let >='0' && let <= '9')) {
+                newstr+=let;
+            }
+            else if ((let >='A' && let <= 'Z')) {
+                newstr+=let+32;
+            }
+            else continue;
+        }
+        // for (auto i : newstr){
+        //     cout << i << ' ';
+        // }
+        // cout << endl;
 
-bool isAlnum(char c) {
-    if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z') || (c >= '0' && c <= '9')) {
+        // is palindrome check
+        int start=0;
+        int end= newstr.size() -1;
+
+        while (start < end){
+            if (newstr[start] != newstr[end]){
+                return false;
+            }
+            start++;
+            end--;
+        }
         return true;
     }
-    return false;
-}
+};
 
-void del_space_and_lowercase(char s[]){
-    int length = strlen(s);
-    char* tmp = (char*)malloc(length + 1); 
-    int k = 0;
-    for (int i = 0; i < length; i++) {
-        if (isAlnum(s[i])) {
-            tmp[k++] = (s[i] >= 'A' && s[i] <= 'Z') ? s[i] + 32 : s[i];
-        }
-    }
-    tmp[k] = '\0';
-    strcpy(s, tmp);
-    free(tmp); 
-}
-
-bool isPalindrome(char* s) {
-    del_space_and_lowercase(s); 
-
-    int start = 0;
-    int end = strlen(s) - 1;
-    while (start < end){
-        if (s[start] != s[end]) {
-            return false;
-        }
-        start += 1;
-        end -= 1;
-    }
-    return true;
-}
