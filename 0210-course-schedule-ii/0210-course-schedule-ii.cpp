dc634eae25dc26e528b31dbb95a8f825e5787f81
Diane Chaeyun Kim <104754233+golddohyun@users.noreply.github.com>
2024-02-28 10:38:44 +0900

    Time: 16 ms (65.37%), Space: 17 MB (50.55%) - LeetHub

--- 0210-course-schedule-ii/0210-course-schedule-ii.cpp
+++ 0210-course-schedule-ii/0210-course-schedule-ii.cpp
@@ -1,33 +1,34 @@
 class Solution {
 public:
     vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) {
+        vector<int> ans;
         vector<vector<int>> adj(numCourses);
-        vector<int> deg(numCourses, 0);
-        vector<int> result;
-        // get adjacent list and indegree list
-        for(auto& i : prerequisites){
-            adj[i[1]].push_back(i[0]);
-            deg[i[0]]++;
-        }
+        vector<int> indegree(numCourses, 0);
         queue<int> Q;
-        for (int i =0; i < numCourses; i++){
-            if(deg[i] == 0) Q.push(i);
-        }
-        
-        for(int i = 0; i < numCourses; i++){
 
-            //cycle exists
-            if (Q.empty()){
-                return {};
-            }
-            int x = Q.front();
-            Q.pop();
-            result.push_back(x);
-            for (int i=0; i < adj[x].size(); i++){
-                int y = adj[x][i];
-                if (--deg[y] == 0) Q.push(y);
-            }
+        // get adjacent listand indegree list
+        for (auto& i : prerequisites){
+            int course  = i[0];
+            int prelim = i[1];
+            adj[prelim].push_back(course);
+            indegree[course]++;
         }
-        return result;
+
+       for (int i=0; i < numCourses; i++) {
+          if (indegree[i] == 0) Q.push(i);
+       }
+
+       while (!Q.empty()){
+          int cur = Q.front(); Q.pop();
+          ans.push_back(cur);
+          for (auto &nei : adj[cur]){
+              if (--indegree[nei] == 0){
+                  Q.push(nei);
+              }
+          }
+       }
+      
+      if (ans.size() != numCourses) return {};
+      return ans;
     }
 };
