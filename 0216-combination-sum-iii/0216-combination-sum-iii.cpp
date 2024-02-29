7dfa4bf0b0f3b11f00463eb9a474adc5eaef2dd0
Diane Chaeyun Kim <104754233+golddohyun@users.noreply.github.com>
2024-02-29 21:56:48 +0900

    Time: 0 ms (100.00%), Space: 8.2 MB (30.13%) - LeetHub

--- 0216-combination-sum-iii/0216-combination-sum-iii.cpp
+++ 0216-combination-sum-iii/0216-combination-sum-iii.cpp
@@ -1,23 +1,27 @@
 class Solution {
 public:
-    void backtrack_comb(int start, const int& k, int target, vector<int>& arr, vector<vector<int>>& res){
-        if (arr.size() == k && target == 0) {
-            res.push_back(arr);
+
+    void backtrack_comb(vector<int>& arr, vector<vector<int>>& result, vector<int>& used, int start, int& maxlength, int target){
+        if (arr.size() == maxlength && target == 0){
+            result.push_back(arr);
             return;
         }
-        for (int num=start; num < 10; num++){
-            if (num <= target){
-                arr.push_back(num);
-                backtrack_comb(num+1, k, target-num, arr, res);
-                arr.pop_back();
-            }
+        for (int i=start; i < 10; i++){
+            if (used[i]) continue;
+            used[i] = 1;
+            arr.push_back(i);
+            backtrack_comb(arr, result, used, i+1, maxlength, target-i);
+            arr.pop_back();
+            used[i] = 0;
         }
     }
+
     vector<vector<int>> combinationSum3(int k, int n) {
-        vector<vector<int>> res;
+        vector<int> used(10, 0);
         vector<int> arr;
-        backtrack_comb(1, k, n, arr, res);
-        return res;
+        vector<vector<int>> result;
+        backtrack_comb(arr, result, used, 1, k, n);
+
+        return result;
     }
 };
-
