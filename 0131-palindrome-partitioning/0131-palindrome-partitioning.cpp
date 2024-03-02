bec361330c7ebebe45ea5e7d770d5f604eb21959
Diane Chaeyun Kim <104754233+golddohyun@users.noreply.github.com>
2024-03-02 16:38:30 +0900

    Time: 157 ms (24.41%), Space: 144.2 MB (17.77%) - LeetHub

--- 0131-palindrome-partitioning/0131-palindrome-partitioning.cpp
+++ 0131-palindrome-partitioning/0131-palindrome-partitioning.cpp
@@ -1,34 +1,35 @@
 class Solution {
 public:
-   bool isPalindrome(string str) {
-    int left = 0, right = str.length() - 1;
-    while (left < right) {
-        if (str[left++] != str[right--])
-            return false;
-    }
-    return true;
-}
-
-void partitionUtil(vector<vector<string>> &res, vector<string> &curr, 
-                   int start, string str) {
-    if (start == str.length()) {
-        res.push_back(curr);
-        return;
-    }
-    for (int i = start; i < str.length(); i++) {
-        if (isPalindrome(str.substr(start, i - start + 1))) {
-            curr.push_back(str.substr(start, i - start + 1));
-            partitionUtil(res, curr, i + 1, str);
-            curr.pop_back();
+    bool is_palindrome(string mystr){
+        int start = 0, end = mystr.size()-1;
+        while (start < end) {
+            if (mystr[start] != mystr[end]) return false;
+            start++;
+            end--;
         }
+        return true;
     }
-}
-
-vector<vector<string>> partition(string str){
-    vector<vector<string>> res;
-    vector<string> curr;
-    partitionUtil(res, curr, 0, str);
-    return res;
-}
-
+    
+    void backtrack(string s, int start, vector<string> path, vector<vector<string>>& result ){
+        if (start == s.size()){
+            result.push_back(path);
+            return;
+        }
+        for (int idx=start; idx < s.size(); idx++){
+            int end = idx;
+            string mysub = s.substr(start, end-start+1);
+            if (is_palindrome(mysub)){
+                path.push_back(mysub);
+                backtrack(s, end+1, path, result);
+                path.pop_back();
+            }
+        }
+        
+    }
+    vector<vector<string>> partition(string s) {
+        vector<string> path;
+        vector<vector<string>> result;
+        backtrack(s, 0, path, result);
+        return result;        
+    }
 };
