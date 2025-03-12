b03cee3bc150d43f91034b316188634e1813a3e0
Diane Chaeyun Kim <104754233+golddohyun@users.noreply.github.com>
2025-03-12 12:45:57 +0900

    Time: 414 ms (99.3%), Space: 20.8 MB (5.04%) - LeetHub

--- 0015-3sum/0015-3sum.py
+++ 0015-3sum/0015-3sum.py
@@ -4,25 +4,24 @@ class Solution(object):
         :type nums: List[int]
         :rtype: List[List[int]]
         """
-        # addressing edge cases
-        if len(nums) < 3 :
-          return []
-        if len(nums) == 3 :
-          return [nums] if sum(nums) == 0 else []
+        if len(nums) < 3:
+            return []
+        if len(nums) == 3:
+            return [nums] if sum(nums) == 0 else []
 
         from collections import Counter
-        result = []
+        result = set()
         counter = Counter(nums)
         nums = list(counter.keys())
         pos_item, neg_item = [x for x in nums if x > 0], [x for x in nums if x < 0]
 
-        if counter[0] >= 3 : result.append([0, 0, 0])
-        for n in neg_item :
-            for p in pos_item :
-                compliment = -(n+p)
-                if compliment in counter :
-                    sorted_sum = sorted([n, p, compliment])
-                    if sorted_sum not in result and ((compliment!=n and compliment!=p) or counter[compliment] > 1) :
-                        result.append(sorted_sum)
+        if counter[0] >= 3:
+            result.add((0, 0, 0))
 
-        return result
+        for n in neg_item:
+            for p in pos_item:
+                compliment = -(n + p)
+                if compliment in counter and ((compliment != n and compliment != p) or counter[compliment] > 1):
+                    result.add(tuple(sorted([n, p, compliment]))) 
+
+        return [list(triplet) for triplet in result] 
